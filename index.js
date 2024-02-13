d3a0d3fa09610d6254badff1e7befa83db58a7ed
bezbrain <belloquadriolawale@gmail.com>
2024-02-13 13:10:43 +0100

    Set up the security middleware

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ const NotFoundMiddleware = require("./middleware/not-found");
 const authRouter = require("./routes/auth.route");
 const connectDB = require("./db/connect");
 
+// Security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per window (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: RateLimit-* headers; draft-7: combined RateLimit header
+    legacyHeaders: false, // Disable the X-RateLimit-* headers.
+    // store: ... , // Use an external store for consistency across multiple server instances.
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 const port = process.env.PORT || 3002;
 
