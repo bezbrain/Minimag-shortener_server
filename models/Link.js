a4996e538ff57e199e24cc4d41cf8350899a58e0
bezbrain <belloquadriolawale@gmail.com>
2024-02-16 12:45:52 +0100

    Create the Link model

---
+++ models/Link.js
@@ -0,0 +1,32 @@
+const { Schema, model, Types } = require("mongoose");
+const nanoId = require("nanoid");
+
+const LinkSchema = new Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: [true, "Please provide original url"],
+    },
+    shortUrl: {
+      type: String,
+      unique: [true, "This url already existed. Please generate another one"],
+    },
+    fullUrl: {
+      type: String,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+LinkSchema.pre("save", async function (next) {
+  this.shortUrl = nanoId.nanoid(7);
+  this.fullUrl = `https://minimag.onrender.com/${this.shortUrl}`;
+  next();
+});
+
+module.exports = model("Link", LinkSchema);
