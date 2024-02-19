63faf113c48140e2d016867c90d08f3a6c26ac44
bezbrain <belloquadriolawale@gmail.com>
2024-02-19 21:03:39 +0100

    Create the endpoint to get all links from DB

--- controllers/linkDetails.controller.js
+++ controllers/linkDetails.controller.js
@@ -1,5 +1,23 @@
+const LinkCollection = require("../models/Link");
+const CusLinkCollection = require("../models/CustomizeLink");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllLinks = async (req, res) => {
-  res.send("Get All Links");
+  const {
+    user: { userId },
+  } = req;
+
+  const links = await LinkCollection.find({ createdBy: userId });
+  const cusLinks = await CusLinkCollection.find({ createdBy: userId });
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    linkCount: links.length,
+    cusLinkCount: cusLinks.length,
+    links,
+    cusLinks,
+    message: "Links successfully fetched",
+  });
 };
 
 module.exports = {
