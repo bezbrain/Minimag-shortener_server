81dbd72bd9eab7973e0b24b7b1dae10bd887703b
bezbrain <belloquadriolawale@gmail.com>
2024-04-01 21:49:08 +0100

    Develop the endpoint to get all customized demo urls

--- routes/demo/demoDetails.route.js
+++ routes/demo/demoDetails.route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   getAllShortDemoLinks,
+  getAllCusDemoLinks,
 } = require("../../controllers/demo/demoDetails.cont");
 
 router.get("/allDemoShortUrl/:demoUserID", getAllShortDemoLinks);
+router.get("/allDemoCusUrl/:demoUserID", getAllCusDemoLinks);
 
 module.exports = router;
